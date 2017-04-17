0e466c0c8cb6cfa73aa670d4a00ffaaafb22149e
Juan Antonio Pérez Clemente <jpc432@inlumine.ual.es>
2017-04-17 18:51:02 +0200

    no message

--- myapplication/src/main/java/com/example/myapplication/Cliente.java
+++ myapplication/src/main/java/com/example/myapplication/Cliente.java
@@ -1,4 +1,17 @@
-public class Cliente {
-	public CabeceraCliente _unnamed_CabeceraCliente_;
-	public ZonaCliente _unnamed_ZonaCliente_;
+package com.example.myapplication;
+
+public class Cliente extends Cliente_V {
+	
+	private ZonaComun zc;
+	
+	public Cliente() {
+		inicializar();
+	}
+
+	private void inicializar() {
+		
+		zc.zonaCliente();
+		
+	}
+
 }
