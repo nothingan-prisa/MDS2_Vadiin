f3dd55b64e4223979d6d7faaa2555ae2abcde18f
Juan <juanrdzbaeza@gmail.com>
2017-04-18 20:48:51 +0200

    nos pasamos al lenovo a media tarde

--- myapplication/src/main/java/com/example/myapplication/ModificarincidenciaAdmin.java
+++ myapplication/src/main/java/com/example/myapplication/ModificarincidenciaAdmin.java
@@ -1,9 +1,12 @@
+package com.example.myapplication;
+
 public class ModificarincidenciaAdmin extends ResumenIncidenciaEnCurso {
-	private Check _estadoC;
-	private TextField _comercialTF;
-	private TextField _fechaTF;
-	private TextArea _descripciónTA;
-	private Button _guardarB;
-	private Button _cancelar;
-	public DetalleIncidenciasAdmin _unnamed_DetalleIncidenciasAdmin_;
+//	private Check _estadoC;
+//	private TextField _comercialTF;
+//	private TextField _fechaTF;
+//	private TextArea _descripciï¿½nTA;
+//	private Button _guardarB;
+//	private Button _cancelar;
+//	public DetalleIncidenciasAdmin _unnamed_DetalleIncidenciasAdmin_;
+	
 }
